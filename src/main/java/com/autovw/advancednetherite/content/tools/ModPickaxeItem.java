11cda68b326a3fce9499b4522dda46bfac4d5ade
Autovw <68228006+Autovw@users.noreply.github.com>
2021-11-27 10:31:53 +0100

    🐛 Fixed ModPickaxeItem extending the wrong class
    Let's forget this ever happened.

--- src/main/java/com/autovw/advancednetherite/content/tools/ModPickaxeItem.java
+++ src/main/java/com/autovw/advancednetherite/content/tools/ModPickaxeItem.java
@@ -1,12 +1,12 @@
 package com.autovw.advancednetherite.content.tools;
 
-import net.minecraft.item.HoeItem;
+import net.minecraft.item.PickaxeItem;
 import net.minecraft.item.ToolMaterial;
 
 /**
  * Author: Autovw
  */
-public class ModPickaxeItem extends HoeItem {
+public class ModPickaxeItem extends PickaxeItem {
     // Can't use the vanilla classes on Fabric because the constructors are protected. Very nice.
     public ModPickaxeItem(ToolMaterial material, int attackDamage, float attackSpeed, Settings settings) {
         super(material, attackDamage, attackSpeed, settings);
