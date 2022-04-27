02a014de62f80eb4828ac823e2a3291e09c896c4
Autovw <68228006+Autovw@users.noreply.github.com>
2022-04-27 15:32:27 +0200

    ✨ Added optional support for Detail Armor Bar mod

---
+++ src/main/java/com/autovw/advancednetherite/client/ClientHandler.java
@@ -0,0 +1,16 @@
+package com.autovw.advancednetherite.client;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.loader.api.FabricLoader;
+
+/**
+ * Author: Autovw
+ */
+public class ClientHandler implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+        if (FabricLoader.getInstance().getModContainer("detailab").isPresent()) {
+            DetailArmorBarSupport.register();
+        }
+    }
+}
