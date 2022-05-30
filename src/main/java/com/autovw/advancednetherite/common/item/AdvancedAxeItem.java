c4c63cd1e504f32a97c8ad7399c3fdf464b42ea5
Autovw <68228006+Autovw@users.noreply.github.com>
2022-05-30 19:01:09 +0200

    🔥 Remove constructor comments
    Point still stands though

--- src/main/java/com/autovw/advancednetherite/common/item/AdvancedAxeItem.java
+++ src/main/java/com/autovw/advancednetherite/common/item/AdvancedAxeItem.java
@@ -7,7 +7,6 @@ import net.minecraft.item.ToolMaterial;
  * Author: Autovw
  */
 public class AdvancedAxeItem extends AxeItem {
-    // Can't use the vanilla classes on Fabric because the constructors are protected. Very nice.
     public AdvancedAxeItem(ToolMaterial material, float attackDamage, float attackSpeed, Settings settings) {
         super(material, attackDamage, attackSpeed, settings);
     }
